17dd097fea421892e03e2a8c4f5f6738dd1ca800
Yuji Ueki <unhappychoice@gmail.com>
2019-03-09 03:16:33 +0900

    :+1: Implement design for IFDOCO

--- src/components/trade/view.ts
+++ src/components/trade/view.ts
@@ -1,6 +1,5 @@
-import {button, div, h4, hr, input, span, VNode} from "@cycle/dom";
+import {br, button, div, h4, hr, input, label, p, span, VNode} from "@cycle/dom";
 import Stream from "xstream";
-import throttle from "xstream/extra/throttle";
 import sampleCombine from "xstream/extra/sampleCombine";
 import {State} from "./index";
 
@@ -35,6 +34,54 @@ export const view = (state$: Stream<State>, boardComponentDOM$: Stream<VNode>, s
                     div(".order-buttons", [
                         button(".clear-button", { attrs: { disabled: state.isOrdering || state.position.size === 0 } }, "Clear Position"),
                         button(".clear-order-button", { attrs: { disabled: state.isOrdering || state.orders.length === 0} }, "Clear Orders"),
+                    ]),
+                    hr(),
+                    h4(".sub-title", "Ranged IFDOCO"),
+                    div(".line", [
+                        label(".label", "Calculated Profit / Loss line"),
+                        p(".line-number", [
+                            p(".buy", [
+                                label(".label", "Buy: "),
+                                span(".profit", "1000000"),
+                                span(" / "),
+                                span(".loss", "1000000")
+                            ]),
+                            p(".sell", [
+                                label(".label", "Sell: "),
+                                span(".profit", "1000000"),
+                                span(" / "),
+                                span(".loss", "1000000")
+                            ])
+                        ]),
+                    ]),
+                    div(".profit-loss", [
+                        label(".label", "Calculated Profit / Loss"),
+                        p(".profit-number", [
+                            p(".buy", [
+                                label(".label", "Buy: "),
+                                span(".profit-line", "10000"),
+                                span(" / "),
+                                span(".loss-line", "10000")
+                            ]),
+                            p(".sell", [
+                                label(".label", "Sell: "),
+                                span(".profit-line", "10000"),
+                                span(" / "),
+                                span(".loss-line", "10000")
+                            ])
+                        ])
+                    ]),
+                    div(".price-width", [
+                        label(".label", "Price width"),
+                        input("#price-width-input", { attrs: { value: state.size }})
+                    ]),
+                    div(".ratio", [
+                        label(".label", "Profit / Loss Ratio %"),
+                        input("#ratio-input", { attrs: { value: state.size }})
+                    ]),
+                    div(".ranged-ifdoco-order-buttons.order-buttons", [
+                        button(".sell-button", { attrs: { disabled: state.isOrdering || state.size === 0 } }, "Sell"),
+                        button(".buy-button", { attrs: { disabled: state.isOrdering || state.size === 0 } },"Buy")
                     ])
                 ])
             ])
